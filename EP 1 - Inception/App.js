56f9e1be283385c48b631d2164e422e5cc9b925c
Sharath-Chandra43 <addanapurisharathchandra99@gmail.com>
2024-01-06 14:20:55 +0530

    Episode-01

---
+++ EP 1 - Inception/App.js
@@ -0,0 +1,17 @@
+
+
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"im an h1 tag"),
+        React.createElement("h2",{},"im an h2 tag"),
+    ]),
+
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"im an h1 tag"),
+        React.createElement("h2",{},"im an h2 tag"),
+    ]),
+)
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
